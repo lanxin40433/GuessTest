1324945f1c436d79db192103ed1c29b502685e38
lanxin40433 <ada40433@gmail.com>
2022-05-31 17:15:02 +0800

    Add guess.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-c
+let min=0;
+let max=100;
+let random = getRandom(0,100);
+console.log(random)
+
+
+guess()
+function guess(){
+    let num = parseInt(prompt("猜猜看" + min + "-" + max));
+    if(isNaN(num)){
+        alert("Not a Number.");
+        guess()
+    }else if(num < min){
+        alert("Number <"+min);
+        guess()
+    }else if(num > max){
+        alert("Number >"+max);
+        guess()
+    }else if(num > random){
+        max = num;
+        guess()
+    }else if(num < random){
+        min = num;
+        guess()
+    }else if(num == random){
+        alert("恭喜 猜對了");
+    }
+}
+
+function getRandom(min,max){
+    return Math.floor(Math.random()*(max-min+1))+min;
+};
+
+
+
